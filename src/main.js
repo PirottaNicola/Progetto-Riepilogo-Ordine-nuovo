d61181a16d0020d2686d7db7d80cdd51d962cacb
abFilippo <Filippo.vetro.3d@gmail.com>
2023-11-23 16:43:01 +0100

    Created class and add function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log('Hello World');
+const costumers = [
+    { id: 100312, firstname: "Mario", lastname: "Rossi", age: 27, email: "Mario.Rossi@gmail.com", phone: 3245522654, locale: "Italia", billingAddress: { street: "Via dei Tulipani", city: "Milano", state: "Italia", postalCode: "20019" }, shippingAddress: { street: "Via dei Tulipani", city: "Milano", state: "Italia", postalCode: "20019" } },
+    { id: 214122, firstname: "Antonio", lastname: "Verdi", age: 47, email: "Antonio.Verdi@gmail.com", phone: 3285643876, locale: "Italia", billingAddress: { street: "Via delle Rose", city: "Torino", state: "Italia", postalCode: "58741" }, shippingAddress: { street: "Via delle Rose", city: "Torino", state: "Italia", postalCode: "58741" } }
+];
+const avaibleProducts = [
+    { code: "A1", name: "Prodotto 1", variants: ["Variante 1", "Variante 2"], description: "Prodotto 1 ........", price: 100, retailer: "Retailer 1" },
+    { code: "A2", name: "Prodotto 2", variants: ["Variante 1", "Variante 2", "Variante 3"], description: "Prodotto 2 ........", price: 120, retailer: "Retailer 2" },
+    { code: "A3", name: "Prodotto 3", variants: ["Variante 1",], description: "Prodotto 3 ........", price: 60, retailer: "Retailer 3" },
+    { code: "A4", name: "Prodotto 4", variants: ["Variante 1", "Variante 2", "Variante 3", "Variante 4"], description: "Prodotto 4 ........", price: 40, retailer: "Retailer 4" },
+    { code: "A5", name: "Prodotto 5", variants: ["Variante 1",], description: "Prodotto 5 ........", price: 21, retailer: "Retailer 5" },
+];
+let selectedProducts;
+// Funzione per aggiungere un prodotto
+function addSelectedProduct(product, quantity) {
+    const lineItemId = selectedProducts.length + 1;
+    const orderLineItem = {
+        id: lineItemId,
+        product,
+        amount: product.price * quantity,
+        quantity
+    };
+    selectedProducts.push(orderLineItem);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,68 @@
-console.log('Hello World')
+interface Customer {
+    id: number;
+    firstname: string;
+    lastname: string;
+    age: number;
+    email: string;
+    phone: number;
+    locale: string;
+    billingAddress: Address;
+    shippingAddress: Address;
+}
+
+interface Address {
+    street: string;
+    city: string;
+    state: string;
+    postalCode: number;
+}
+
+interface Order {
+    orderNumber: string;
+    orderDate: Date;
+    totalAmount: number;
+    OrderLineItems: OrderLineItems[];
+}
+
+interface OrderLineItems {
+    id: number;
+    product: Product;
+    amount: number;
+    quantity: number;
+}
+
+interface Product {
+    code: string;
+    name: string;
+    variants: string[];
+    description: string;
+    price: number;
+    retailer: string;
+}
+
+const costumers: Customer[] = [
+    {id: 100312, firstname: "Mario", lastname:"Rossi", age: 27, email:"Mario.Rossi@gmail.com", phone:3245522654, locale:"Italia", billingAddress:{street:"Via dei Tulipani",city:"Milano",state:"Italia",postalCode:"20019" }, shippingAddress:{street:"Via dei Tulipani",city:"Milano",state:"Italia",postalCode:"20019" }},
+    {id: 214122, firstname: "Antonio", lastname:"Verdi", age: 47, email:"Antonio.Verdi@gmail.com", phone:3285643876, locale:"Italia", billingAddress:{street:"Via delle Rose",city:"Torino",state:"Italia",postalCode:"58741"}, shippingAddress:{street:"Via delle Rose",city:"Torino",state:"Italia",postalCode:"58741" }}
+]
+
+const avaibleProducts: Product[] = [
+    { code: "A1", name: "Prodotto 1", variants: ["Variante 1", "Variante 2"], description: "Prodotto 1 ........", price: 100, retailer: "Retailer 1" },
+    { code: "A2", name: "Prodotto 2", variants: ["Variante 1", "Variante 2", "Variante 3"], description: "Prodotto 2 ........", price: 120, retailer: "Retailer 2" },
+    { code: "A3", name: "Prodotto 3", variants: ["Variante 1",], description: "Prodotto 3 ........", price: 60, retailer: "Retailer 3" },
+    { code: "A4", name: "Prodotto 4", variants: ["Variante 1", "Variante 2", "Variante 3", "Variante 4"], description: "Prodotto 4 ........", price: 40, retailer: "Retailer 4" },
+    { code: "A5", name: "Prodotto 5", variants: ["Variante 1",], description: "Prodotto 5 ........", price: 21, retailer: "Retailer 5" },
+]
+
+let selectedProducts: OrderLineItems[];
+
+// Funzione per aggiungere un prodotto
+function addSelectedProduct(product: Product, quantity: number): void{
+    const lineItemId = selectedProducts.length + 1;
+    const orderLineItem : OrderLineItems = {
+        id: lineItemId,
+        product,
+        amount: product.price * quantity,
+        quantity
+    };
+    selectedProducts.push(orderLineItem);
+}
