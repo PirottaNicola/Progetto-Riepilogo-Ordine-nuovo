de9b41a304afaa7a4e1f9f119114beb846811a4c
Nicola Pirotta <nicolapirotta@gmail.com>
2023-11-24 11:21:17 +0100

    inizio funzione render

---
+++ src/helper.ts
@@ -0,0 +1,10 @@
+//* funzione render:
+// prende in input un array di oggetti e l'elemento html in cui renderizzarli
+
+function render(array: any[], element: HTMLElement): void {
+  let html = ''
+  array.forEach((item) => {
+    html += `<li>${item}</li>`
+  })
+  element.innerHTML = html
+}
